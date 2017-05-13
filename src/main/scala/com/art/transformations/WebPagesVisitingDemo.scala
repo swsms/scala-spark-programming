3c17f7f7ae4cb47a611ae3372e636b1b09d44664
art <swsms@mail.ru>
2017-05-13 23:43:27 +0500

    Added join and cogroup examples

---
+++ src/main/scala/com/art/transformations/WebPagesVisitingDemo.scala
@@ -0,0 +1,30 @@
+package com.art.transformations
+
+import com.art.SparkInitializer
+import org.apache.spark.SparkContext
+
+object WebPagesVisitingDemo extends App {
+
+  val sc: SparkContext = SparkInitializer.init("numbers-processing-demo")
+
+  val visits = sc.parallelize(Seq(
+    ("index.html", "1.2.3.4"),
+    ("about.html", "3.4.5.6"),
+    ("index.html", "22.22.22.22"),
+    ("faq.html", "1.2.3.4")
+  ))
+
+  val pageNames = sc.parallelize(Seq(
+    ("index.html", "Home"),
+    ("about.html", "About"),
+    ("faq.html", "FAQ")
+  ))
+
+  val joined = visits.join(pageNames).collect().toList
+  val cogrouped = visits.cogroup(pageNames).collect().toList
+
+  println(s"Joined: $joined")
+  println(s"Cogrouped: $cogrouped")
+
+  sc.stop()
+}
